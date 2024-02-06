836e58713c008948cfbc9ccbe850978e3b5c72d0
AmolGayakwad <158410064+AmolGayakwad@users.noreply.github.com>
2024-02-06 09:07:31 +0530

    Update tempConverter.js

--- tempConverter.js
+++ tempConverter.js
@@ -1,7 +1,9 @@
+//Write a program that converts temperatures between Celsius and Fahrenheit. The user should input a temperature and the unit (C or F), and the program should output the converted temperature.
+
 const celsius = prompt("Enter a celsius value: ");
 
-// calculate fahrenheit
+// formula to calculate the fahrenheit
 const fahrenheit = (celsius * 1.8) + 32;
 
-// display the result
+
 console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
